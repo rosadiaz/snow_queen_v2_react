d7333780a86fd22a5d15ebbb96ec300943417b75
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-11-28 12:33:29 -0800

    Adds polygon and rectangle drawing tool

--- rails-snow-queen/app/assets/javascripts/application.js
+++ rails-snow-queen/app/assets/javascripts/application.js
@@ -23,6 +23,27 @@ function initMap() {
     mapTypeId: google.maps.MapTypeId.SATELLITE,
     tilt:0
   });
+
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: ['polygon', 'rectangle']
+    },
+    markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+  });
+
+  drawingManager.setMap(map);
+
   let geocoder = new google.maps.Geocoder();
 
   document.getElementById('AddressSearch').addEventListener('submit', function(event) {
