a12ba5c2c7a91bfaa66574b922b3c3cf2ffa2cd7
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-11-28 20:57:44 -0800

    Refactored js files

--- rails-snow-queen/app/assets/javascripts/application.js
+++ rails-snow-queen/app/assets/javascripts/application.js
@@ -10,68 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
 
-let marker = null;
-
 function initMap() {
-  let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
-    center: {lat: 49.2860, lng: -122.8130},
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    tilt:0
-  });
-
-  const drawingOptions = {
-    fillColor: '#2DC1D6',
-    fillOpacity: .3,
-    strokeWeight: 5,
-    strokeColor: '#2DC1D6',
-    clickable: false,
-    editable: true,
-    zIndex: 1
-  }
-  let drawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.POLYGON,
-    drawingControl: true,
-    drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: ['polygon', 'rectangle']
-    },
-    // markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
-    // markerOptions: {icon: 'snow_flake.png'},
-    polygonOptions: drawingOptions,
-    rectangleOptions: drawingOptions
-  });
-
-  drawingManager.setMap(map);
-
-  let geocoder = new google.maps.Geocoder();
-
-  document.getElementById('AddressSearch').addEventListener('submit', function(event) {
-    event.preventDefault();
-    if (marker) { marker.setMap(null) }
-    geocodeAddress(geocoder, map);
-  });
-}
-
-function geocodeAddress(geocoder, resultsMap) {
-  let address = document.getElementById('address').value;
-  geocoder.geocode({'address': address, 'region': "CA"}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.panTo(results[0].geometry.location);
-      resultsMap.setZoom(20);
-      marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-      console.log("===========================================")
-      console.log(address)
-      console.log("===========================================")
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
+  new Map();
 }
---
+++ rails-snow-queen/app/assets/javascripts/map.js
@@ -0,0 +1,98 @@
+class Map {
+	constructor() {
+    this.map = this.initMap();
+    this.drawingManager = this.initDrawingManager();
+    this.removeControl = this.initRemoveControl();
+    this.geocoder = this.initGeocoder();
+    this.marker = null;
+    this.geocodedAddress = null;
+    this.polygon = null;
+
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+    this.handleGeocodingResponse = this.handleGeocodingResponse.bind(this);
+    this.addListeners()
+  }
+  
+  initMap() {
+    return new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: {lat: 49.2860, lng: -122.8130},
+      mapTypeId: google.maps.MapTypeId.SATELLITE,
+      tilt:0
+    });
+  }
+  
+  initDrawingManager() {
+    const drawingOptions = {
+      fillColor: '#2DC1D6',
+      fillOpacity: 0.3,
+      strokeWeight: 5,
+      strokeColor: '#2DC1D6',
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+    return new google.maps.drawing.DrawingManager({
+      drawingMode: google.maps.drawing.OverlayType.POLYGON,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: ['polygon', 'rectangle']
+      },
+      map: this.map,
+      // markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+      // markerOptions: {icon: 'snow_flake.png'},
+      polygonOptions: drawingOptions,
+      rectangleOptions: drawingOptions
+    })
+  }
+  
+  initRemoveControl() {
+    const removeControlDiv = document.createElement('div');
+    removeControlDiv.classList.add("map-btn")
+    removeControlDiv.title = 'Click to remove selected area from the map';
+    removeControlDiv.innerHTML = 'Remove selection';
+
+    removeControlDiv.index = 1;
+    this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(removeControlDiv);
+
+    return removeControlDiv;
+  }
+
+  initGeocoder() {
+    return new google.maps.Geocoder();
+  }
+
+  addListeners() {
+    document.getElementById('AddressSearch').addEventListener('submit', this.handleSearchSubmit);
+    this.removeControl.addEventListener('click', function() {
+      // polygon.setMap(null);
+      console.log("remove polygon")
+    });
+  }
+  
+  handleSearchSubmit(event) {
+    event.preventDefault();
+    if (this.marker) { this.marker.setMap(null) }
+    this.geocodeAddress();
+  }
+
+  geocodeAddress() {
+    let address = document.getElementById('address').value;
+    this.geocoder.geocode({'address': address, 'region': "CA"}, this.handleGeocodingResponse);
+  }
+
+  handleGeocodingResponse(results, status) {
+    if (status === 'OK') {
+      this.map.panTo(results[0].geometry.location);
+      this.map.setZoom(20);
+      this.marker = new google.maps.Marker({
+        map: this.map,
+        position: results[0].geometry.location
+      });
+      this.geocodedAddress = results[0].formatted_address;
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  }
+}
