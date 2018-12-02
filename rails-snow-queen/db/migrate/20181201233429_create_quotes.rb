b219aa5ae565c7631745635903ff9cee64a43de6
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-12-01 18:51:57 -0800

    Creates quotes model and adds simple_form gem

---
+++ rails-snow-queen/db/migrate/20181201233429_create_quotes.rb
@@ -0,0 +1,15 @@
+class CreateQuotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :quotes do |t|
+      t.string :address
+      t.float :area
+      t.decimal :total
+      t.string :email
+      t.text :comments
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
