ba9176726ebbd4f790b64f84f406c40049fd8923
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-12-04 10:55:54 -0800

    adds index to email

--- rails-snow-queen/db/schema.rb
+++ rails-snow-queen/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_01_233429) do
+ActiveRecord::Schema.define(version: 2018_12_04_185121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,13 +18,15 @@ ActiveRecord::Schema.define(version: 2018_12_01_233429) do
   create_table "quotes", force: :cascade do |t|
     t.string "address"
     t.float "area"
-    t.decimal "total"
+    t.float "total"
     t.string "email"
     t.text "comments"
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "polygons"
+    t.index ["email"], name: "index_quotes_on_email"
   end
 
 end
