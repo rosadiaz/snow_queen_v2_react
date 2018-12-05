173310a135528eed62da0795e5874aea0e0af6ec
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-12-05 10:31:14 -0800

    Improves layout and colors

--- rails-snow-queen/app/assets/javascripts/quotingPanel.js
+++ rails-snow-queen/app/assets/javascripts/quotingPanel.js
@@ -14,16 +14,21 @@ class QuotingPanel {
   showAddress(geocodedAddress) {
     if (geocodedAddress) {
       this.geocodedAddress = geocodedAddress;
-      const addressNode = document.getElementById("displayAddress");
+      const primaryAddressNode = document.getElementById("primaryAddress");
+      const secondaryAddressNode = document.getElementById("secondaryAddress");
       let splitAddress = geocodedAddress.split(",");
-      while (addressNode.firstChild) { addressNode.removeChild(addressNode.firstChild) }
-      splitAddress.forEach(element => {
+      while (primaryAddressNode.firstChild) { primaryAddressNode.removeChild(primaryAddressNode.firstChild) }
+      while (secondaryAddressNode.firstChild) { secondaryAddressNode.removeChild(secondaryAddressNode.firstChild) }
+      splitAddress.forEach((element, i) => {
         const div = document.createElement("div");
         div.innerText = element;
-        addressNode.appendChild(div);
+        if (i === 0) { 
+          primaryAddressNode.appendChild(div) 
+        } else {
+          secondaryAddressNode.appendChild(div);
+        }
       });
-      addressNode.classList.remove("hidden");
-      document.getElementById("searchAddressButton").classList.remove("hidden");
+      document.getElementById("displayAddress").classList.remove("hidden");
 
       const addressNodeInModal = document.getElementById("addressModal");
       addressNodeInModal.innerText = splitAddress;
